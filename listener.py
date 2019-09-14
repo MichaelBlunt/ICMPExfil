cd322cb4327bf71b3b26c08c0175ed16a2bb7b6d
Michael <43708524+MichaelBlunt@users.noreply.github.com>
2019-09-14 09:55:07 -0400

    Create listener.py
    Very basic python program for listening for the ICMP packets.

---
+++ listener.py
@@ -0,0 +1,14 @@
+import socket
+import sys
+
+def recv():
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    while True:
+        data, src = s.recvfrom(1508)
+        payload = data[44:60]
+        sys.stdout.write(payload)
+
+
+if __name__ == '__main__':
+    recv()
+The Sender
